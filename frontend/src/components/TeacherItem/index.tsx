ff39cfbc21dd21e0c3aec2bd8bbdb8c9dcc179bb
vitormuuniz <vitormuuniz@gmail.com>
2020-08-07 11:36:02 -0300

    adding connection button counter

--- frontend/src/components/TeacherItem/index.tsx
+++ frontend/src/components/TeacherItem/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
-
 export interface Teacher {
-    id: number;
-    avatar: string;
-    bio: string;
-    cost: number;
-    name: string;
-    subject: string;
-    whatsapp: string;
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
 }
 
 interface TeacherItemProps {
@@ -22,13 +22,16 @@ interface TeacherItemProps {
 const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({
   teacher,
 }) => {
+  function createNewConnection() {
+    api.post('/connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src={teacher.avatar}
-          alt={teacher.name}
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
           <strong>{teacher.name}</strong>
           <span>{teacher.subject}</span>
@@ -42,7 +45,11 @@ const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({
           Preço/hora
           <strong>R$ {teacher.cost}</strong>
         </p>
-        <a href={`https://wa.me/${teacher.whatsapp}`}>
+        <a
+          target="blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
         </a>
